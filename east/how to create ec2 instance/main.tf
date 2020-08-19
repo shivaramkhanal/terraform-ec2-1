63f075766ce3f19d80a9ced8cc1ed296c329580b
Rupesh2010 <rupesh.phuyal1986@gmail.com>
2020-08-19 19:27:23 -0400

    Update main.tf

--- east/how to create ec2 instance/main.tf
+++ east/how to create ec2 instance/main.tf
@@ -6,12 +6,9 @@ resource "aws_instance" "web1" {
   ami = "${var.ami_image}"
   instance_type = "${var.instance_type}"
   key_name = "${var.key_name}"
-  security_groups = ["${var.security_groups}"]
+  security_groups = "${var.security_groups}"
   user_data       = "${file("userdata.sh")}"
   iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
-  tags {
-    Name = "Instance1"
-  }
  }
 
 resource "aws_iam_instance_profile" "test_profile" {                             
