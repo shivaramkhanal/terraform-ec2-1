25ef3f5aa46a16d985fa41dcaf568d83284ac641
shivaramkhanal <55666186+shivaramkhanal@users.noreply.github.com>
2020-08-24 12:49:28 -0400

    Update main.tf

--- east/how to create ec2 instance/main.tf
+++ east/how to create ec2 instance/main.tf
@@ -1,16 +1,16 @@
-configuration file:
 provider "aws" {
- 
-  region   = "us-east-1"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web3" {
+  ami = "${var.ami_image}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = ["sg-017011d5ea42dde9b"]
+  user_data       = "${file("userdata.sh")}"
+  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
  }
- 
-resource "aws_instance" "example" {
- 
-  ami         = "ami-02354e95b39ca8dec"
-  instance_type = "t2.micro"
- 
-  tags {
- 
-  name = "TESTVM"
- 
-  }
+
+resource "aws_iam_instance_profile" "test_profile" {                                                    
+role = "admin"
+}  
