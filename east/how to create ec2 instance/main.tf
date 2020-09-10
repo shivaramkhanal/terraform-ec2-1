2f9bf8f91599ddbc876bed1502ba937fc4b4f23f
shivaramkhanal <55666186+shivaramkhanal@users.noreply.github.com>
2020-09-09 23:47:50 -0400

    Update main.tf

--- east/how to create ec2 instance/main.tf
+++ east/how to create ec2 instance/main.tf
@@ -18,7 +18,7 @@ resource "aws_iam_instance_profile" "test_profile" {
 resource "aws_security_group" "securitygr" {
   name        = "security"
   description = "create security group"
-  vpc_id      = "default"
+  vpc_id      = var.vpc_id
   
 
   ingress {
