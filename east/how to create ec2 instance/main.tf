a6ad41456a2d9df53ce596725708d10065fbbec9
shivaramkhanal <55666186+shivaramkhanal@users.noreply.github.com>
2020-09-09 22:20:39 -0400

    Update main.tf

--- east/how to create ec2 instance/main.tf
+++ east/how to create ec2 instance/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web3" {
   ami = "${var.ami_image}"
   instance_type = "${var.instance_type}"
   key_name = "${var.key_name}"
-  vpc_security_group_ids = ["sg-a97497e5"]
+  vpc_security_group_ids = ["${aws_security_group.securitygr.name}"]
   user_data       = "${file("userdata.sh")}"
   iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
  }
@@ -14,3 +14,52 @@ resource "aws_instance" "web3" {
 resource "aws_iam_instance_profile" "test_profile" {                                                    
 #role = "admin"
 }  
+
+resource "aws_security_group" "securitygr" {
+  name        = "security"
+  description = "create security group"
+  vpc_id      = "default"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
