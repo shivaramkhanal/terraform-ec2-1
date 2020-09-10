cdc395628ca5f95b4135b32a52f9eb3ec3e9a769
shivaramkhanal <55666186+shivaramkhanal@users.noreply.github.com>
2020-09-09 21:38:51 -0400

    Update main.tf

--- east/how to create ec2 instance/main.tf
+++ east/how to create ec2 instance/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web3" {
   ami = "${var.ami_image}"
   instance_type = "${var.instance_type}"
   key_name = "${var.key_name}"
-  vpc_security_group_ids = ["sg-018e7a460567847b1"]
+  vpc_security_group_ids = ["sg-a97497e5"]
   user_data       = "${file("userdata.sh")}"
   iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
  }
