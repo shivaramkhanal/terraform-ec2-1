7f036871ac1af253a088c84de612dc07c72c7d0f
shivaramkhanal <55666186+shivaramkhanal@users.noreply.github.com>
2020-09-10 11:27:25 -0400

    Create main1.tf

---
+++ east/how to create ec2 instance/main1.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "securitygr" {
+  name        = "security2"
+  description = "create security group"
+  vpc_id      = var.vpc_id
+  
+  
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
